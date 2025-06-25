306493133b03154797a8aa12e3bea25104dae90e
VitaliiK <whitetech0047@gmail.com>
2025-06-25 21:12:21 +0200

    HW-Lesson14. Implementation of all methods in the PostController class

--- src/main/java/ait/cohort5860/post/service/PostServiceImpl.java
+++ src/main/java/ait/cohort5860/post/service/PostServiceImpl.java
@@ -1,57 +1,57 @@
-package ait.cohort5860.post.service;
-
-import ait.cohort5860.post.dto.NewCommentDto;
-import ait.cohort5860.post.dto.NewPostDto;
-import ait.cohort5860.post.dto.PostDto;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.List;
-
-@Service
-public class PostServiceImpl implements PostService {
-    @Override
-    public PostDto addNewPost(String author, NewPostDto newPostDto) {
-        return null;
-    }
-
-    @Override
-    public PostDto findPostById(Long id) {
-        return null;
-    }
-
-    @Override
-    public PostDto updatePost(Long id, NewPostDto newPostDto) {
-        return null;
-    }
-
-    @Override
-    public PostDto addComment(Long id, String commenter, NewCommentDto newCommentDto) {
-        return null;
-    }
-
-    @Override
-    public PostDto deletePost(Long id) {
-        return null;
-    }
-
-    @Override
-    public void addLike(Long id) {
-
-    }
-
-    @Override
-    public List<PostDto> findPostsByAuthor(String author) {
-        return List.of();
-    }
-
-    @Override
-    public List<PostDto> findPostsByTags(List<String> tags) {
-        return List.of();
-    }
-
-    @Override
-    public List<PostDto> findPostsByPeriod(LocalDate dateFrom, LocalDate dateTo) {
-        return List.of();
-    }
-}
+//package ait.cohort5860.post.service;
+//
+//import ait.cohort5860.post.dto.NewCommentDto;
+//import ait.cohort5860.post.dto.NewPostDto;
+//import ait.cohort5860.post.dto.PostDto;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//@Service
+//public class PostServiceImpl implements PostService {
+//    @Override
+//    public PostDto addNewPost(String author, NewPostDto newPostDto) {
+//        return null;
+//    }
+//
+//    @Override
+//    public PostDto findPostById(Long id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public PostDto updatePost(Long id, NewPostDto newPostDto) {
+//        return null;
+//    }
+//
+//    @Override
+//    public PostDto addComment(Long id, String commenter, NewCommentDto newCommentDto) {
+//        return null;
+//    }
+//
+//    @Override
+//    public PostDto deletePost(Long id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void addLike(Long id) {
+//
+//    }
+//
+//    @Override
+//    public List<PostDto> findPostsByAuthor(String author) {
+//        return List.of();
+//    }
+//
+//    @Override
+//    public List<PostDto> findPostsByTags(List<String> tags) {
+//        return List.of();
+//    }
+//
+//    @Override
+//    public List<PostDto> findPostsByPeriod(LocalDate dateFrom, LocalDate dateTo) {
+//        return List.of();
+//    }
+//}
